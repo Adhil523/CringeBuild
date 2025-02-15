7318050f3b08504f61fd8b2a47acb514760450e3
Adhil523 <adhilrumais007@gmail.com>
2025-02-15 12:13:29 +0530

    modal sample

--- adich-mone/src/lib/modals.ts
+++ adich-mone/src/lib/modals.ts
@@ -1,3 +1,5 @@
 import type { ModalComponent } from '@skeletonlabs/skeleton';
 
-export const modalRegistry: Record<string, ModalComponent> = {};
+export const modalRegistry: Record<string, ModalComponent> = {
+	// 'modalID':{ref:ModalImport}
+};
